d5788bef44a1a95cef0a3ec88087509a91ae7b53
yash1022 <dyashsingh.4@gmail.com>
2025-04-13 22:14:25 +0530

    feat: Implement logout functionality in auth routes and NavBar component

--- FRONTEND/vite-project/src/Components/NavBar.jsx
+++ FRONTEND/vite-project/src/Components/NavBar.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/NavBar.css'; // Updated path to NavBar.css
+import api from '../../axiosConfig';
 
 const NavBar = () => {
+
+  const user = localStorage.getItem('user');
+  const handleLogout = () => {
+    api.post('/auth/logout') // Call the logout API endpoint
+    .then(() => {
+      console.log("Logged out successfully");
+    })
+    localStorage.removeItem('user'); // Clear user data from local storage
+    window.location.reload(); // Reload the page to reflect the changes
+  }
+
 
   return (
     <nav className="nav-bar">
@@ -12,7 +24,7 @@ const NavBar = () => {
           <li><Link to="/hospitals">Hospitals</Link></li>
           <li><Link to="/blood-availability">Blood Availability</Link></li>
           <li><Link to="/appointments">Appointments</Link></li>
-          <li><Link to="/login">Login</Link></li>
+          <li>{!user?(<Link to="/login">Login</Link>):(<button onClick={handleLogout}>Logout</button>)}</li>
 
          
         </ul>
