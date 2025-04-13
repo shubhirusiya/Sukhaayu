83943534d1cef70804a36b3312fab894040af572
yash1022 <150256255+yash1022@users.noreply.github.com>
2025-04-14 02:48:26 +0530

    Update axiosConfig.js

--- FRONTEND/vite-project/axiosConfig.js
+++ FRONTEND/vite-project/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://techies-amuhacks4-0-2.onrender.com',
   withCredentials: true,
  
 });
